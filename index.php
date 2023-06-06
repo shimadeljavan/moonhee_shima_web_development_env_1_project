118c1b5ed086c4f7a614076c8621b971b04eb540
shimadeljavan <134743007+shimadeljavan@users.noreply.github.com>
2023-06-06 18:14:48 -0400

    add

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@600&family=Rubik:wght@300&display=swap"
+        rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@300&family=Rubik:wght@300&display=swap"
+        rel="stylesheet">
+
+    <link rel="stylesheet" href="assets/css/main.css">
+    <title>Shima_Deljavan-03</title>
+</head>
+
+<body>
+
+    <section class="big-banner">
+        <div class="text-box">
+            <h1>enjoy the ocean with your family</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae nemo placeat ex nihil quidem earum odio
+                minima a quibusdam, repellend.</p>
+            <a class="btn1" href="#">read more</a>
+        </div>
+    </section>
+
+    <section class="about">
+        <div class="container">
+            <div class="about-flex">
+                <div class="left"></div>
+                <div class="right">
+                    <h2>welcome to our website</h2>
+                    <div class="bar"></div>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis asperiores, repellat,
+                        repellendus consequatur.
+                        <br>
+                        <br>
+                        unde numquam? Ipsum et odit officia reiciendis optio, distinctio accusantium, quia. Molestias,
+                        non molestiae.
+                    </p>
+                    <a class="btn2" href="#">learn more</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="cta">
+        <div class="container">
+            <h2>get in touch with us</h2>
+            <p>Lorem, ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            <a class="btn3" href="#">contact us</a>
+        </div>
+    </section>
+
+</body>
+
+</html>
