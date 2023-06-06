bfc395e406e4b72cad0f81c7b841b63fdf43551a
shimadeljavan <134743007+shimadeljavan@users.noreply.github.com>
2023-06-06 18:40:43 -0400

    about us

---
+++ about.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>About us</title>
+</head>
+
+<body>
+
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis asperiores, repellat, repellendus consequatur.
+
+        unde numquam? Ipsum et odit officia reiciendis optio, distinctio accusantium, quia. Molestias, non molestiae.
+    </p>
+</body>
+
+</html>
