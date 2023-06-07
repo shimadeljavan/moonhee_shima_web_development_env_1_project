c0e3e32516792c00a508831b6cdae517ec7db3eb
shimadeljavan <134743007+shimadeljavan@users.noreply.github.com>
2023-06-07 12:45:11 -0400

    monn

--- index.php
+++ index.php
@@ -1,61 +1,78 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html>
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="./assets/main.css">
 
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@600&family=Rubik:wght@300&display=swap"
-        rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@300&family=Rubik:wght@300&display=swap"
-        rel="stylesheet">
+    <!-- <link rel="stylesheet" href="assets/css/home.css"> -->
 
-    <link rel="stylesheet" href="./assets/css/main.css">
-    <title>Shima_Deljavan-03</title>
 </head>
 
 <body>
 
-    <section class="big-banner">
-        <div class="text-box">
-            <h1>enjoy the ocean with your family</h1>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae nemo placeat ex nihil quidem earum odio
-                minima a quibusdam, repellend.</p>
-            <a class="btn1" href="about.php">About Us</a>
-        </div>
-    </section>
+    <!-- <button><a href="index.php">Home Page</a></button> -->
+    <!-- <button><a href="about.php">About</a></button> -->
+    <!-- <button><a href="contact.php">Contact</a></button> -->
 
-    <section class="about">
-        <div class="container">
-            <div class="about-flex">
-                <div class="left"> </div>
-                <div class="right">
-                    <h2>welcome to our website</h2>
-                    <div class="bar"></div>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis asperiores, repellat,
-                        repellendus consequatur.
-                        <br>
-                        <br>
-                        unde numquam? Ipsum et odit officia reiciendis optio, distinctio accusantium, quia. Molestias,
-                        non molestiae.
-                    </p>
-                    <a class="btn2" href="about.php">About Us</a>
+    <header>
+        <div class="row">
+            <div class="col">
+                <img class="style-logo" src="images/logo-sm.png" alt="SM Logo">
+                <nav class="style-nav">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <div class="row">
+            <div class="col">
+                <div class="hero">
+                    <h4>discover & connect with great places around the world</h4>
+                    <h1>Find your perfect place</h1>
+                    <a href="" class="button">Read More</a>
                 </div>
             </div>
         </div>
-    </section>
-
-    <section class="cta">
-        <div class="container">
-            <h2>get in touch with us</h2>
-            <p>Lorem, ipsum, dolor sit amet consectetur adipisicing elit.</p>
-            <a class="btn3" href="#">contact us</a>
+        <div class="row">
+            <div class="col col-md-one-half">
+                <div class="info-box-tan">
+                    <h2>Inspiring Reads</h2>
+                    <p>The future is in lightening our footprints and brightening our lives.</p>
+                    <a href="" class="button">Read More</a>
+                </div>
+            </div>
+            <div class="col col-md-one-half">
+                <div class="info-box-dark">
+                    <h2>Reflections</h2>
+                    <p>Stylish ways to bounce the heat off a roof and the planet.</p>
+                    <a href="" class="button">Read More</a>
+                </div>
+            </div>
         </div>
-    </section>
+        <div class="row">
+            <div class="col col-md-one-half col-lg-one-fourth"><img src="images/gallery-1.jpg" alt=""></div>
+            <div class="col col-md-one-half col-lg-one-fourth"><img src="images/gallery-2.jpg" alt=""></div>
+            <div class="col col-md-one-half col-lg-one-fourth"><img src="images/gallery-3.jpg" alt=""></div>
+            <div class="col col-md-one-half col-lg-one-fourth"><img src="images/gallery-4.jpg" alt=""></div>
+        </div>
+    </main>
+
+    <footer>
+        <div class="row">
+            <div class="col">
+                <p class="footer-text">©2023 Moonhee & Shima All rights reserved.</p>
+                <p class="footer-text">This is a final project in the Class of Web Development Environment</p>
+            </div>
+        </div>
+    </footer>
 
 </body>
 
