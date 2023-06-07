4ec9fd41f2624d2ecf4531fd178974ec5817dc10
shimadeljavan <134743007+shimadeljavan@users.noreply.github.com>
2023-06-07 13:06:44 -0400

    about

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
                 <nav class="style-nav">
                     <ul>
                         <li><a href="#">Home</a></li>
-                        <li><a href="#">About</a></li>
+                        <li><a href="about.php">About</a></li>
                         <li><a href="#">Contact</a></li>
                     </ul>
                 </nav>
