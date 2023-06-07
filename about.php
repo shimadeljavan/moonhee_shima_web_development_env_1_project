985ff8f9a78f97f9e843da455c9440e107f96cfc
shimadeljavan <134743007+shimadeljavan@users.noreply.github.com>
2023-06-06 20:46:28 -0400

    image

--- about.php
+++ about.php
@@ -7,8 +7,16 @@
 
 <body>
 
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis asperiores, repellat, repellendus consequatur.
+    <h1>About US:</h1>
+    <hr>
 
+    <img src="assets/images/a.jpg" alt="photo" height="20px" width="20px">
+
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.<br>
+        Facilis asperiores, repellat, repellendus consequatur.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi nulla repudiandae ea saepe assumenda,
+        <br>eos debitis qui nihil. Illo quia dignissimos explicabo, accusantium aliquid beatae reprehenderit officiis
+        distinctio totam deserunt?
         unde numquam? Ipsum et odit officia reiciendis optio, distinctio accusantium, quia. Molestias, non molestiae.
     </p>
 </body>
