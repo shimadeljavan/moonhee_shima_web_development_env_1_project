f276930225ce00b23587be9825a75082571bd27b
shimadeljavan <134743007+shimadeljavan@users.noreply.github.com>
2023-06-07 13:48:54 -0400

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>contact us</title>
+</head>
+
+<body>
+    <h1>Contact Us</h1>
+    <hr>
+    <h3> shima.deljavan@gmail.com <br>
+        moonhee.kim@gmail.com....
+    </h3>
+</body>
+
+</html>
