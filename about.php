bcb30cb8c5cb44c935f58c4f8b5a1228e5798077
Moonheeui <lovekmhrosa@gmail.com>
2023-06-08 17:47:08 -0400

    Update

--- about.php
+++ about.php
@@ -1,61 +1,58 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About page</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>About page</title>
 
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Figtree:wght@500&family=Poltawski+Nowy&family=Poppins:wght@300&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Figtree:wght@500&family=Poltawski+Nowy&family=Poppins:wght@300&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" type="text/css" href="about.css">
+  <link rel="stylesheet" type="text/css" href="about.css">
 
 </head>
-
 <body>
-    <header class="main-header">
-        <div class="container">
-            <div class="header-flex">
-                <div class="logo">
-                    <a href="#"><img src="images/logo-sm.png" alt="#"></a>
-                </div>
-                <nav>
-                    <ul class="nav-list">
-                        <li>
-                            <a href="">Home</a>
-                        </li>
-                        <li>
-                            <a href="">About</a>
-                        </li>
-                        <li>
-                            <a href="">Contact</a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
+  <header class="main-header">
+    <div class="container">
+      <div class="header-flex">
+        <div class="logo">
+          <a href="#"><img src="images/logo-sm.png" alt="#"></a>
         </div>
-    </header>
+        <nav>
+          <ul class="nav-list">
+            <li>
+              <a href="">Home</a>
+            </li>
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </header>
 
-    <section class="banner">
-        <div class="container">
-            <div class="banner-flex">
-                <div class="left">
-                    <h3>ABOUT US</h3>
-                    <span class="divider"></span>
-                    <h2>We design and build better Website, for a better life</h2>
-                    <p>
-                        Our work can involve design, programming (front-end and back-end), databases, publishing and project management. Graduates will be able to successfully integrate into the labour market and into multidisciplinary teams.
-                    </p>
-                    <a class="btn" href="#">More Information</a>
-                </div>
-
-                <div class="right"></div>
-            </div>
+  <section class="banner">
+    <div class="container">
+      <div class="banner-flex">
+        <div class="left">
+          <h3>ABOUT US</h3>
+          <span class="divider"></span>
+          <h2>We design and build better Website, for a better life</h2>
+          <p>
+            Our work can involve design, programming (front-end and back-end), databases, publishing and project management. Graduates will be able to successfully integrate into the labour market and into multidisciplinary teams.
+          </p>
+          <a class="btn" href="#">More Information</a>
         </div>
-    </section>
 
+        <div class="right"></div>
+      </div>
+    </div>
+  </section>
+  
 </body>
-
 </html>
--- contact.php
+++ contact.php
@@ -1,66 +1,63 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact page</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Contact page</title>
 
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Figtree:wght@500&family=Poltawski+Nowy&family=Poppins:wght@300&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Figtree:wght@500&family=Poltawski+Nowy&family=Poppins:wght@300&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" type="text/css" href="contact.css">
+  <link rel="stylesheet" type="text/css" href="contact.css">
 
 </head>
-
 <body>
-    <header class="main-header">
-        <div class="container">
-            <div class="header-flex">
-                <div class="logo">
-                    <a href="#"><img src="images/logo-sm.png" alt="#"></a>
-                </div>
-                <nav>
-                    <ul class="nav-list">
-                        <li>
-                            <a href="">Home</a>
-                        </li>
-                        <li>
-                            <a href="">About</a>
-                        </li>
-                        <li>
-                            <a href="">Contact</a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
+  <header class="main-header">
+    <div class="container">
+      <div class="header-flex">
+        <div class="logo">
+          <a href="#"><img src="images/logo-sm.png" alt="#"></a>
         </div>
-    </header>
+        <nav>
+          <ul class="nav-list">
+            <li>
+              <a href="">Home</a>
+            </li>
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </header>
 
-    <section class="banner">
-        <div class="container">
-            <div class="banner-flex">
-                <div class="left">
-                    <h2>Moonhee Kim</h2>
-                    <span class="divider"></span>
-                    <h3>2295442@edu.vaniercollege.qc.ca</h3>
-                    <a class="btn" href="#">Contact Me</a>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <h2>Shima Deljavan Farshi</h2>
-                    <span class="divider"></span>
-                    <h3>2395094@edu.vaniercollege.qc.ca</h3>
-                    <a class="btn" href="#">Contact Me</a>
-                </div>
-
-                <div class="right"></div>
-            </div>
+  <section class="banner">
+    <div class="container">
+      <div class="banner-flex">
+        <div class="left">
+          <h2>Moonhee Kim</h2>
+          <span class="divider"></span>
+          <h3>2295442@edu.vaniercollege.qc.ca</h3>
+          <a class="btn" href="#">Contact Me</a>
+          <br>
+          <br>
+          <br>
+          <br>
+          <h2>Shima Deljavan Farshi</h2>
+          <span class="divider"></span>
+          <h3>2395094@edu.vaniercollege.qc.ca</h3>
+          <a class="btn" href="#">Contact Me</a>
         </div>
-    </section>
 
+        <div class="right"></div>
+      </div>
+    </div>
+  </section>
+  
 </body>
-
 </html>
