9a3a8581526ee896037c5520e821c465426d7b07
Moonheeui <lovekmhrosa@gmail.com>
2023-06-08 17:30:28 -0400

    Update about.php and contact.php
    I updated 2 php files

--- about.php
+++ about.php
@@ -4,13 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact page</title>
+    <title>About page</title>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Figtree:wght@500&family=Poltawski+Nowy&family=Poppins:wght@300&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" type="text/css" href="contact.css">
+    <link rel="stylesheet" type="text/css" href="about.css">
 
 </head>
 
@@ -42,18 +42,13 @@
         <div class="container">
             <div class="banner-flex">
                 <div class="left">
-                    <h2>Moonhee Kim</h2>
+                    <h3>ABOUT US</h3>
                     <span class="divider"></span>
-                    <h3>2295442@edu.vaniercollege.qc.ca</h3>
-                    <a class="btn" href="#">Contact Me</a>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <h2>Shima Deljavan Farshi</h2>
-                    <span class="divider"></span>
-                    <h3>2395094@edu.vaniercollege.qc.ca</h3>
-                    <a class="btn" href="#">Contact Me</a>
+                    <h2>We design and build better Website, for a better life</h2>
+                    <p>
+                        Our work can involve design, programming (front-end and back-end), databases, publishing and project management. Graduates will be able to successfully integrate into the labour market and into multidisciplinary teams.
+                    </p>
+                    <a class="btn" href="#">More Information</a>
                 </div>
 
                 <div class="right"></div>
