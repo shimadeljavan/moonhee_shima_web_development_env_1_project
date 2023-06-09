af12ed3c003c716eae7117ff8c509ae05fbd8c84
Moonheeui <lovekmhrosa@gmail.com>
2023-06-09 13:23:48 -0400

    update all file

---
+++ index.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="./assets/css/main.css">
+
+</head>
+
+<body>
+
+    <header>
+        <div class="row">
+            <div class="col">
+                <img class="style-logo" src="/assets/images/logo-sm.png" alt="SM Logo">
+                <nav class="style-nav">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="about.php">About</a></li>
+                        <li><a href="contact.php">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <div class="row">
+            <div class="col">
+                <div class="hero">
+                    <h4>Discover & Connect with Great Places around the World</h4>
+                    <h1>Find Your Perfect Place</h1>
+                    <a href="" class="button">Read More</a>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col col-md-one-half">
+                <div class="info-box-tan">
+                    <h2>MOONHEE KIM</h2>
+                    <p>Web Designer <br><br> Works only on frontend HTML, CSS, Photoshop</p>
+                    <a href="" class="button">Read More</a>
+                </div>
+            </div>
+            <div class="col col-md-one-half">
+                <div class="info-box-dark">
+                    <h2>Shima Deljavan Farshi</h2>
+                    <p>Web Designer <br><br> Works only on frontend HTML, CSS, PHP</p>
+                    <a href="" class="button">Read More</a>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col col-md-one-half col-lg-one-fourth"><img src="/assets/images/gallery-1.jpg" alt=""></div>
+            <div class="col col-md-one-half col-lg-one-fourth"><img src="/assets/images/gallery-2.jpg" alt=""></div>
+            <div class="col col-md-one-half col-lg-one-fourth"><img src="/assets/images/gallery-3.jpg" alt=""></div>
+            <div class="col col-md-one-half col-lg-one-fourth"><img src="/assets/images/gallery-4.jpg" alt=""></div>
+        </div>
+    </main>
+
+    <footer>
+        <div class="row">
+            <div class="col">
+                <p class="footer-text">©2023 Moonhee & Shima All rights reserved.</p>
+                <p class="footer-text">This is a final project in the Class of Web Development Environment</p>
+            </div>
+        </div>
+    </footer>
+
+</body>
+
+</html>
